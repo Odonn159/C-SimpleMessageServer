ff57a549b0a811855f6a8847af589aee3f2fde3d
Emmett O. <98720396+Odonn159@users.noreply.github.com>
2024-09-01 13:12:03 -0500

    Add files via upload

---
+++ MessengerClient/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static TcpClient _client;
+    private static NetworkStream _stream;
+
+    static async Task Main(string[] args)
+    {
+        string serverIp = "127.0.0.1"; // Server IP address
+        int serverPort = 12345;        // Server port
+
+        _client = new TcpClient();
+        await _client.ConnectAsync(serverIp, serverPort);
+        _stream = _client.GetStream();
+        Console.WriteLine("Connected to server.");
+        Console.WriteLine("Type 'quit' or 'q' to exit");
+        _ = Task.Run(() => ReceiveMessagesAsync());
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == null) continue;
+            if (message == "quit"){
+                break;
+            }
+            if (message == "q"){
+                break;
+            }
+            var data = Encoding.UTF8.GetBytes(message);
+            await _stream.WriteAsync(data, 0, data.Length);
+        }
+    }
+
+    private static async Task ReceiveMessagesAsync()
+    {
+        var buffer = new byte[1024];
+        while (true)
+        {
+            int bytesRead = await _stream.ReadAsync(buffer, 0, buffer.Length);
+            if (bytesRead == 0) break;
+
+            var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received: {message}");
+        }
+    }
+}
---
+++ MessengerServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static TcpListener _listener;
+    private static List<TcpClient> _clients = new List<TcpClient>();
+
+    static async Task Main(string[] args)
+    {
+        int port = 12345; // Port for the server
+        _listener = new TcpListener(IPAddress.Any, port);
+        _listener.Start();
+        Console.WriteLine($"Server started on port {port}.");
+
+        while (true)
+        {
+            var client = await _listener.AcceptTcpClientAsync();
+            _clients.Add(client);
+            Console.WriteLine("Client connected.");
+            _ = Task.Run(() => HandleClientAsync(client));
+        }
+    }
+
+    private static async Task HandleClientAsync(TcpClient client)
+    {
+        using (var stream = client.GetStream())
+        {
+            var buffer = new byte[1024];
+            while (true)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0) break;
+
+                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received: {message}");
+
+                // Broadcast message to all connected clients
+                foreach (var c in _clients)
+                {
+                    if (c != client)
+                    {
+                        var data = Encoding.UTF8.GetBytes(message);
+                        await c.GetStream().WriteAsync(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+
+        _clients.Remove(client);
+        client.Close();
+        Console.WriteLine("Client disconnected.");
+    }
+}
